f0aac25e1e2ebfe975d592895d64b3e51627e9c5
Nick Dyer <nbdyer@gmail.com>
2015-01-05 08:06:56 +0000

    Refactor database and model out of server.js

--- server.js
+++ server.js
@@ -7,10 +7,11 @@
     var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
     var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
     var port = process.env.PORT || 3000;
+    var database = require('./config/database');
 
     // configuration =================
 
-    mongoose.connect('mongodb://localhost/todo');     // connect to mongoDB database on modulus.io
+    mongoose.connect(database.url);     // connect to mongoDB database on modulus.io
 
     app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
@@ -18,11 +19,6 @@
     app.use(bodyParser.json());                                     // parse application/json
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
     app.use(methodOverride());
-
-    var Todo = mongoose.model('Todo', {
-      text : String,
-      done : Boolean
-    });
 
     // routes ================================
     
